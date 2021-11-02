0bcd770116ae5ec692e6e3484adc259cf7b0577f
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2021-11-03 00:41:01 +0100

    Add query for food.com recipes

--- queries/tarql_query.bash
+++ queries/tarql_query.bash
@@ -1,1 +1,3 @@
 tarql -H ./queries/all_recipes.sparql ./datasets/all_recipes.csv > ./results/all_recipes.ttl
+
+tarql ./queries/food_recipes.sparql ./datasets/food_recipes.csv > ./results/food_recipes.ttl
