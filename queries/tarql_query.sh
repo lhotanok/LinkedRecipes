6f1fe10cf8ee530608cdc4ec04d34613c7da86d0
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2021-11-03 02:53:20 +0100

    Move source datasets into source directory, add text description

--- queries/tarql_query.sh
+++ queries/tarql_query.sh
@@ -1,5 +1,5 @@
-tarql -H ./queries/all_recipes.sparql ./datasets/all_recipes.csv > ./results/all_recipes.ttl
+tarql -H ./queries/all_recipes.sparql ./source/all_recipes.csv > ./results/all_recipes.ttl
 
-tarql ./queries/food_recipes.sparql ./datasets/food_recipes.csv > ./results/food_recipes.ttl
+tarql ./queries/food_recipes.sparql ./source/food_recipes.csv > ./results/food_recipes.ttl
 
-tarql ./queries/bbc_recipes.sparql ./datasets/bbc_recipes.csv > ./results/bbc_recipes.ttl
+tarql ./queries/bbc_recipes.sparql ./source/bbc_recipes.csv > ./results/bbc_recipes.ttl
